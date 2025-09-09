df5f14f8919d0d46baf6a50df70597bc5a1e4d05
John Niyontwali <nijohn0006@gmail.com>
2025-09-09 03:27:56 +0200

    refine eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,17 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
     rules: {
-      "@next/next/no-img-element": "off",
+      // Next.js specific rules - set as warnings
+      "@next/next/no-css-tags": "warn",
+      "@next/next/no-html-link-for-pages": "warn", 
+      "@next/next/no-page-custom-font": "warn",
+      "@next/next/no-img-element": "warn",
+      
+      // React rules - set as warnings
+      "react/no-unescaped-entities": "warn",
+      
+      // Accessibility rules - set as warnings
+      "jsx-a11y/alt-text": "warn",
     },
   },
   {
